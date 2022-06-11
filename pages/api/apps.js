12581652c4a2853521017d8b2560decb692d6dda
AbbanFahim <lloydtechno58@gmail.com>
2022-06-11 20:29:14 +0400

    added notezzz

--- pages/api/apps.js
+++ pages/api/apps.js
@@ -1,5 +1,14 @@
 const array = [
     {
+        img: '/notezzz.png',
+        title: "Notezzz",
+        desc: "A cross platform note-taking app",
+        links: [
+            {type: "globe", link: "https://notezzz.web.app"},
+            {type: "github", link: "https://github.com/Abban-Fahim/notezzz"}
+        ]
+    },
+    {
         img: '/ez-mcq.png', 
         title: "EZ MCQs", 
         desc: "A little app to help you prepare for the IGCSE Science MCQs",
@@ -8,15 +17,6 @@ const array = [
             {type: "github", link: "https://github.com/Abban-Fahim/MCQ"}
         ]
     },
-    {
-        img: '/notezzz.png',
-        title: "Notezzz",
-        desc: "A cross platform note-taking app",
-        links: [
-            {type: "globe", link: "https://notezzz.web.app"},
-            {type: "github", link: "https://github.com/Abban-Fahim/notezzz"}
-        ]
-    }
 ];
 
 export default function (req, res) {
