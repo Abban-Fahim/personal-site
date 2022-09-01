73ecd4e1715d31ff015a74734a50d65beebe3869
AbbanFahim <lloydtechno58@gmail.com>
2022-09-01 10:42:47 +0400

    added 404 page and redirection

--- src/App.svelte
+++ src/App.svelte
@@ -4,21 +4,36 @@
   import Logo from "./components/abban-logo.svg";
   import ContactPage from "./pages/ContactPage.svelte";
   import Home from "./pages/Home.svelte";
+  import NotFound from "./pages/404.svelte";
+
+  let navElement;
+  let navIcon;
+
+  function toggleNav() {
+    navElement.classList.toggle("hidden");
+    navIcon.classList.toggle("hidden");
+  }
 </script>
 
 <Router basepath="/personal-site">
   <header>
     <Link to="/" id="logo"><img src={Logo} alt="Logo" /></Link>
-    <nav>
+    <button type="button" id="collapser" on:click={toggleNav}
+      ><i bind:this={navIcon} class="bi bi-plus hidden" /></button
+    >
+    <nav bind:this={navElement}>
       <Link to="work">My projects</Link>
       <Link to="contact">Contact</Link>
-      <a href="https://abban-fahim.github.io/blog/">My Blog</a>
+      <a target="__blank" href="https://abban-fahim.github.io/blog/">My Blog</a>
+      <div>
+        <a target="__blank" href="https://github.com/Abban-Fahim/"><i class="bi bi-github" /></a>
+        <a target="__blank" href="https://www.instagram.com/abbanfahim__/"><i class="bi bi-instagram" /></a>
+        <a target="__blank" href="https://www.linkedin.com/in/abban-fahim/"><i class="bi bi-linkedin" /></a>
+      </div>
     </nav>
   </header>
   <Route component={Projects} path="/work" />
   <Route component={ContactPage} path="/contact" />
   <Route component={Home} path="/" />
+  <Route component={NotFound} />
 </Router>
-
-<style>
-</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/personal-site/",
   plugins: [svelte()],
+  optimizeDeps: { exclude: ["svelte-navigator"] },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "404.html"),
+      },
+    },
+  },
 });
